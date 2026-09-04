1791ff76c7c42b431ebdda0bc704289446cc6b5b
fexsec <165517769+fexsecc@users.noreply.github.com>
2026-09-04 19:18:18 +0300

    Fix clipboard for wsl

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -2,7 +2,6 @@ vim.wo.number = true
 vim.opt.tabstop = 4         -- Number of spaces that a <Tab> counts for
 vim.opt.shiftwidth = 4      -- Number of spaces to use for each step of (auto)indent
 vim.opt.expandtab = true    -- Use spaces instead of tabs
-vim.o.clipboard = 'unnamedplus' -- Sync clipboard with OS
 vim.o.wrap = true
 vim.o.linebreak = true         -- wrap at word boundaries
 vim.o.showbreak = "↳ "         -- prefix for wrapped lines
@@ -12,6 +11,23 @@ vim.o.autoindent = true
 vim.o.ignorecase = true -- Ignore case for search
 vim.o.smartcase = true
 vim.o.cursorline = true
+
+-- Sync clipboard with OS
+if vim.fn.has('wsl') == 1 then
+  vim.g.clipboard = {
+    name = 'win32yank',
+    copy = {
+      ['+'] = 'win32yank.exe -i --lf',
+      ['*'] = 'win32yank.exe -i --lf',
+    },
+    paste = {
+      ['+'] = 'win32yank.exe -o --lf',
+      ['*'] = 'win32yank.exe -o --lf',
+    },
+    cache_enabled = 0,
+  }
+end
+vim.opt.clipboard = "unnamedplus"
 
 -- Unmap F1
 vim.keymap.set({"n","i","v","o","t"}, "<F1>", "<Nop>", {silent = true})
